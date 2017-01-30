e2a3ec09715af2c56599f94cd300dec45a7da7f7
Anton Swartz <djnecrogami@gmail.com>
2017-01-30 13:27:12 -0500

    fixing template typo

--- resources/views/install.blade.php
+++ resources/views/install.blade.php
@@ -3,6 +3,6 @@ chmod 700 $HOME/.ssh
 touch $HOME/.ssh/authorized_keys
 chmod 600 $HOME/.ssh/authorized_keys
 @foreach ($keys as $key)
-echo "# {{ $key['keyname'] }}
+echo "# {{ $key['keyname'] }}" >> $HOME/.ssh/authorized_keys
 echo "{{ $key['key'] }}" >> $HOME/.ssh/authorized_keys
 @endforeach
